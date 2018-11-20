d683b1b3d55ea7d060bc0467545c24f5df6aa928
Maddy O'Brien Jones <35735580+maddyobrienjones@users.noreply.github.com>
2018-11-19 22:17:29 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,6 +6,7 @@ import csv
 import json
 import pickle
 
+#contains all movie codes, scraped from genre pages of imdb --> urls formatted like imdb.com/title/'code'
 with open('codes.pkl', 'rb') as f:
     codes = pickle.load(f)
 
@@ -13,41 +14,44 @@ movie_links = ['https://www.imdb.com/title/'+x for x in codes]
 
 def extract_info(url):
     response = requests.get(url)
+    #if works, proceed
     if response.status_code==200:
         page = response.text
         soup = BeautifulSoup(page, 'lxml')
-
+        #for some pages, data stored in json format --> easier to parse
         info = soup.find('script', attrs={'type':'application/ld+json'}).text
         info = json.loads(info)
-
-        try:
+        
+        #scraping data with try, except clause as many of the pages have slightly different formats
+        
+        try: #if json
             title = info['name']
-        except:
+        except: #search in html
             try:
                 title = soup.find('title').text.split('(')[0].strip()
             except:
                 title = None
 
-        try:
+        try: #if json
             rating = info['aggregateRating']['ratingValue']
-        except:
+        except: #search in html
             try:
                 rating = soup.find('span', attrs={'itemprop':'ratingValue'}).text
             except:
                 rating = None
 
-        try:
+        try: #if json
             reviewcount = info['aggregateRating']['ratingCount']
-        except:
+        except: #search in html
             try:
                 reviewcount = soup.find('span', attrs={'class':'small', 'itemprop':'ratingCount'}).text
                 reviewcount = int(reviewcount.replace(',', ''))
             except:
                 reviewcount = None
 
-        try:
+        try: #if json
             director = info['director']['name']
-        except:
+        except: #search in html
             try:
                 if type(info['director'])==list:
                     director = list()
@@ -56,19 +60,19 @@ def extract_info(url):
                     director = ', '.join(director)
                 else:
                     pass
-            except:
+            except: #listed multiple times or in different places on some pages
                 try:
                     director = soup.find(text=re.compile('Director:')).findNext().text
                 except:
                     director = None
 
-        try:
+        try: #if json
             mpaa= info['contentRating']
         except:
             mpaa = None
 
-        try:
-            if info['genre']:
+        try: #if json
+            if info['genre']: #make sure all genres are captured
                 genres = info['genre']
                 if type(genres)==list:
                     genre1 = genres[0]
@@ -86,36 +90,37 @@ def extract_info(url):
             genre2 = None
             genre3 = None
 
-        try:
+        try: #search in html
             releasedate = soup.find('a', attrs={'title':'See more release dates'}).text.split('(')[0].strip()
             releasedate = pd.to_datetime(releasedate)
         except:
             releasedate = None
 
-        try:
+        try: #search in html
             runtime = soup.find(text=re.compile('Runtime:')).findNext().text.split()[0]
             runtime = int(runtime)
         except:
-            try:
+            try: #if json, does not always work correctly
                 runtime = info['duration']
-                runtime = re.findall(r'\d+', runtime)
-                runtime = int(runtime[0])*60 + int(runtime[1])
+                runtime = re.findall(r'\d+', runtime) #formatted as 1H30M
+                #need to multiply first number by 60 and add second number to get runtime as minutes
+                runtime = int(runtime[0])*60 + int(runtime[1])             
             except:
                 runtime = None
 
-        try:
+        try: #search in html
             language = soup.find(text=re.compile('Language:')).findNext().text
         except:
             language = None
 
-        try:
+        try: #search in html
             country = soup.find(text=re.compile('Country:')).findNext().text
         except:
             country = None
 
-        try:
+        try: #search in html
             budget = soup.find(text=re.compile('Budget:')).parent.parent.text
-            try:
+            try: #parsing out number
                 budget = str(budget).split('$')[1].split('(')[0].replace(',','')
                 budget = int(budget)
             except:
@@ -123,9 +128,9 @@ def extract_info(url):
         except:
             budget = None
 
-        try:
+        try: #search in html
             cumwwgross = soup.find(text=re.compile('Cumulative Worldwide Gross:')).parent.parent.text
-            try:
+            try: #clean
                 cumwwgross = str(cumwwgross).split('$')[1].replace(',','').split()[0]
                 cumwwgross = int(cumwwgross)
             except:
@@ -133,16 +138,17 @@ def extract_info(url):
         except:
             cumwwgross = None
 
-        try:
+        try: #search in html
             grossusa = soup.find(text=re.compile('Gross USA:')).parent.parent.text
-            try:
+            try: #clean
                 grossusa = str(grossusa).split('$')[1].replace(',','').split()[0]
                 grossusa = int(grossusa)
             except:
                 pass
         except:
             grossusa = None
-
+        
+        #creating dictionary for each movie to append to list
         movie = {
         'title': title,
         'user_rating': rating,
@@ -168,6 +174,7 @@ def extract_info(url):
 
 lod = []
 
+#creating list of dicts to turn into dataframe
 for each in movie_links:
     row = extract_info(each)
     lod.append(row)
